e3ee5339dbe28aed89c5ec0a0d4c702dbc7e73b5
Khalid Saleem <ksaleem@degdigital.com>
2019-12-10 12:13:08 -0500

    Optimization

--- source/js/components/search-form.js
+++ source/js/components/search-form.js
@@ -1,98 +1,106 @@
 import fetchUtils from "@degjs/fetch-utils";
+import formMapper from "@degjs/form-mapper";
 
 const searchForm = () => {
 
 
     const formEl = document.querySelector('.js-search-form');
-    const layoutEl = document.querySelector('.js-general-content')
+    const layoutEl = document.querySelector('.js-general-content');
+    const searchInputEl = formEl.querySelector('.js-search');
+    const searchButtonEl = formEl.querySelector('.js-search-btn');
 
     const url = formEl.dataset.api;
 
     function init() {
-        formEl.addEventListener('submit', e => {
-            e.preventDefault();
-            getData();
-        });
+        formEl.addEventListener('submit', searchSubmit);
     }
 
+    function searchSubmit(e) {
+        e.preventDefault();
+        getData();
+    }
 
     function getData() {
-        const formEls = formEl.querySelectorAll('.js-value');
+        preloader(true);
         fetchUtils.getJSON(
             url,
             {
-                body: JSON.stringify(formEls),
+                body: JSON.stringify(formMapper.getValues(searchInputEl)),
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 }
-            },
-            {
-                timeout: 10000
             }
         )
         .then((data) => {
+            preloader(false);
             renderResults(data.results);
         })
         .catch(error => {
+            preloader(false);
             alert(error);
         });
     }
 
+    function preloader(isSearching) {
+        if(isSearching === true) {
+            searchButtonEl.value = 'Searching...';
+            searchButtonEl.disabled = true;
+        } else {
+            searchButtonEl.value = 'Search';
+            searchButtonEl.disabled = false; 
+        }
+    }
+
     function renderLocations(item) {
         return `<ul class="doctor-location">
-                    ${item.locations.map(item => {
-                        return `<li class="doctor-location__item">
-                                    <a href="${item.url}" class="doctor-location__link">${item.name}</a>
-                                    <div class="doctor-location__distance">${item.distance.toFixed(1)} Miles</div>
-                                </li>`
-                    }).join('')}
+                    ${item.locations.map(item => 
+                        `<li class="doctor-location__item">
+                            <a href="${item.url}" class="doctor-location__link">${item.name}</a>
+                            <div class="doctor-location__distance">${item.distance.toFixed(1)} Miles</div>
+                        </li>`).join('')}
                 </ul>`
     }
 
 
     function renderSpecialties(item) {
         return `<ul class="specialties-list">
-                    ${item.specialties.map(item => {
-                        return `<li class="specialties-list__item">${item}</li>`
-                    }).join('')}
+                    ${item.specialties.map(item => `<li class="specialties-list__item">${item}</li>`).join('')}
                 </ul>`
     }
 
-    function renderTotalNumberOfResults(data) {
-        const totalNumbers = data.length;
-        const totalResults =  `<h2 class="doctors-total-result">Total Results: <span>${totalNumbers}</span></h2>`
-        layoutEl.insertAdjacentHTML('afterbegin', totalResults);
-    }
 
-    function renderDoctorsList(data) {
-        const renderDoctors = data.map(item => {
-            return `<li class="doctor-list__item"> 
-                        <div class="doctor-info">
-                            <div class="doctor-info__general">
-                                <div class="doctor-image">
-                                    <img src="${item.image}" alt="">
-                                </div>
-                                <div class="doctor-general">
-                                    <h3 class="doctor-name"><a href="${item.url}" target="_blank">${item.fullName}</a></h3>
-                                    ${renderSpecialties(item)}
-                                </div>
-                            </div>
-                            <div class="doctor-info__location">
-                                ${renderLocations(item)}
-                            </div>
-                        </div>
-                    </li>`;
-        }).join('');
 
-        layoutEl.insertAdjacentHTML('beforeend', `<ul class="doctor-list">${renderDoctors}</ul>`);
+    function renderDoctorsSearchResult(data) {
+        const totalNumberOfDoctors = data.length;
         
+        return `<h2 class="doctors-total-result">Total Results: <span>${totalNumberOfDoctors}</span></h2>
+                <ul class="doctor-list">
+                    ${data.map(item => 
+                        `<li class="doctor-list__item"> 
+                                <div class="doctor-info">
+                                    <div class="doctor-info__general">
+                                        <div class="doctor-image">
+                                            <img src="${item.image}" alt="">
+                                        </div>
+                                        <div class="doctor-general">
+                                            <h3 class="doctor-name"><a href="${item.url}" target="_blank">${item.fullName}</a></h3>
+                                            ${renderSpecialties(item)}
+                                        </div>
+                                    </div>
+                                    <div class="doctor-info__location">
+                                        ${renderLocations(item)}
+                                    </div>
+                                </div>
+                            </li>`
+                    ).join('')}
+                </ul>`;        
     }
 
     function renderResults(data) {
         layoutEl.innerHTML = '';
-        renderDoctorsList(data);
-        renderTotalNumberOfResults(data);
+        layoutEl.innerHTML = renderDoctorsSearchResult(data);
+
     }
 
     init();

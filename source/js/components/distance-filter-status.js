e99d3392b9f9bf06ad76ea33c26c7e1d6558fb28
Khalid Saleem <ksaleem@degdigital.com>
2020-04-30 13:45:52 -0400

    just commit

---
+++ source/js/components/distance-filter-status.js
@@ -0,0 +1,20 @@
+const updateDistanceFilterStatus = (settings) => {
+
+    console.log('called');
+
+    const filterActiveDistanceEl = document.querySelector('.js-active-distance');
+    const filterZipCodeEl = document.querySelector('.js-active-zipcode');
+
+    function init() {
+        updateDistanceFilterValues();
+    }
+
+    function updateDistanceFilterValues() {
+        filterActiveDistanceEl.innerHTML = settings.distanceVal;
+        filterZipCodeEl.innerHTML = settings.zipCodeVal;
+    }
+
+    init();
+}
+
+export default updateDistanceFilterStatus;
---
+++ source/js/components/distance-filter.js
@@ -0,0 +1,27 @@
+
+const distanceFilter = (settings) => {
+
+    const rangeEl = document.querySelector('.js-range');
+
+    function init() {
+        rangeEl.addEventListener('change', onRangeChange);
+    }
+
+    function onRangeChange(event) {
+        const distance = event.target.value;
+        settings.onFilterChange({distance});
+    }
+
+    function disable(isDisabled) {
+        rangeEl.disabled = isDisabled;
+    }
+
+    init();
+
+    return {
+        disable
+    }
+}
+
+
+export default distanceFilter;
